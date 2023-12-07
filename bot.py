9b0cce55a1014719a2dae85102afa81e8401e034
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-12-07 14:46:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,21 +13,8 @@ async def aexec(code, client, message):
 from pyrogram import Client
 
 #variables
-
-
-bot = Client("QuantumRobot", bot_token="6910428877:AAFIFbleAgAtf42tNQuty-gRbl4ybWIIPCQ", api_id=10187126, api_hash="ff197c0d23d7fe54c89b44ed092c1752")
-#yourVariables Here↓
-OWN = 5965055071 #OWNERID
-CHAT_M = " " #MainGroupID
-B_NAME = "QuantumRoBot" #Botname
-OWN_NAME = "Otazuki" #Ownername
-OWN_USRNAMER = "Otazuki" #OwnerUsername
-DEV = "6916220465" #Add DEV Id
-ODEV = [OWN, DEV]
-B_F_N = "𝙌𝙪𝙖𝙣𝙩𝙪𝙢𝙍𝙤𝘽𝙤𝙩" #Bot in font
-S_URL = "FutureCity005" #Support Group url
-
-#---------
+from vars import *
+#END
 
 #Imports
 from datetime import datetime

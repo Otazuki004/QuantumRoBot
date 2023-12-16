9e308193aabe9a44fe4d85359d2a2c8d7f0b8e13
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-12-16 17:43:53 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,13 +1,16 @@
 a_id = "10187126" # Your Api Id
 a_hash = "ff197c0d23d7fe54c89b44ed092c1752" # Your Api Hash 
 b_tok = "6910428877:AAFIFbleAgAtf42tNQuty-gRbl4ybWIIPCQ" # Your Bot Token
-#yourVariables Here↓
 OWN = 5965055071 #OWNERID
-CHAT_M = " " #MainGroupID
+CHAT_S_ID = " " #supportchatid
 B_NAME = "QuantumRoBot" #Botname
 OWN_NAME = "Otazuki" #Ownername
 OWN_USRNAMER = "Otazuki" #OwnerUsername
 DEV = "6916220465" #Add DEV Id
 ODEV = [OWN, DEV]
-B_F_N = "𝙌𝙪𝙖𝙣𝙩𝙪𝙢𝙍𝙤𝘽𝙤𝙩" #Bot in font
+B_F_N = "𝙌𝙪𝙖𝙣𝙩𝙪𝙢𝙍𝙤𝘽𝙤𝙩" #Bot Name in font
 S_URL = "FutureCity005" #Support Group url
+B_ID = "" #Botid
+n_boot = "NO" #Do you need Boot exinternal OS? if Yes Add "YES"
+data_col = True #Leave it
+data_store_id = "None" #Leave it because i need it HEHE

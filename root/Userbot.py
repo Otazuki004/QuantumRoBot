fb386a49036e8f0bd5064c3d65f7985b40b631b3
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-12-18 20:57:38 +0530

    Delete root/Userbot.py

--- root/Userbot.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import *
-from root.bot import *
-from root import *
-
-#UBModuleStart
-@UB.on_message(filters.regex(".alive"))
-def Alive_UB (UB, message):
-    UB.send_message(message.chat.id, "I'm Alive")
-#UBEND

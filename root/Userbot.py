eae332f9342aa7e06b93e08e49fc7e71ba6751f9
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-12-18 17:48:01 +0530

    Update Userbot.py

--- root/Userbot.py
+++ root/Userbot.py
@@ -2,4 +2,9 @@ from pyrogram import *
 from root.bot import *
 from vars import *
 
+#UBModuleStart
+@UB.on_message(filters.regex(".alive"))
+def Alive_UB (UB, message):
+    UB.send_message(message.chat.id, "I'm Alive")
+#UBEND
 UB.run()

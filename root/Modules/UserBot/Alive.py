bac81aa22fddff66eee52d2fb47433466b367109
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-12-18 20:57:00 +0530

    Create Alive.py

---
+++ root/Modules/UserBot/Alive.py
@@ -0,0 +1,9 @@
+from pyrogram import *
+from root.__main__ import *
+from root import *
+
+#UBModuleStart
+@UB.on_message(filters.command("alive") & prefix=PRE)
+def Alive_UB (UB, message):
+    UB.send_message(message.chat.id, "I'm Alive")
+#UBEND

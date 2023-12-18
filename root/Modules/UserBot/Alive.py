2a04578ef35acd37f233786c6122ba4252e9fe1a
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-12-18 21:07:50 +0530

    Update Alive.py

--- root/Modules/UserBot/Alive.py
+++ root/Modules/UserBot/Alive.py
@@ -3,7 +3,7 @@ from root.__main__ import *
 from root import *
 
 #UBModuleStart
-@UB.on_message(filters.command("alive") & prefix=PRE)
+@UB.on_message(filters.command("alive") & filters.prefix(PRE))
 def Alive_UB (UB, message):
     UB.send_message(message.chat.id, "I'm Alive")
 #UBEND

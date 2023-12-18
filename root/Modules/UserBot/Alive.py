154f1905d391169b63c04f5e5da9f05519bace75
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-12-18 21:11:54 +0530

    Delete root/Modules/UserBot/Alive.py

--- root/Modules/UserBot/Alive.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import *
-from root.__main__ import *
-from root import *
-
-#UBModuleStart
-@UB.on_message(filters.command("alive") & filters.prefix(PRE))
-def Alive_UB (UB, message):
-    UB.send_message(message.chat.id, "I'm Alive")
-#UBEND

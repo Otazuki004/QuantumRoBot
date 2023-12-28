da814f3245e8663a9e295f54001182bad28b9217
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-12-28 15:02:25 +0530

    Update Message_remover.py

--- root/plugins/Message_remover.py
+++ root/plugins/Message_remover.py
@@ -1,8 +1,16 @@
+import re
 from pyrogram import filters
 from root.__main__ import bot
-import re 
 
-@bot.on_message(filters.text & filters.regex(r"\bpunda\b", re.IGNORECASE))
+# List of words to filter
+bad_words = [
+    "fuck", "Kena", "Mairu", "Sex", "Girlfriendsex", "Mf", "Tf", "Wtf", "Ass", 
+    "Boobs", "Kunji", "Punda", "Otha", "Umbu", "Coolip", "Drugs", "Saaraku", 
+    "Ponnupunda", "Motherfucker", "Fucker", "Fuckers", "Pundaboys", "Sunni", 
+    "MadanGowri", "AdamGaming", "Ff", "Mr"
+]
+
+@bot.on_message(filters.text & filters.regex(fr"\b(?:{'|'.join(map(re.escape, bad_words))})\b", re.IGNORECASE))
 async def remove_message(_, message):
     try:
         await message.delete()

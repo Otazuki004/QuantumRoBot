31a91ae1b91e1f7f0b66dac23b99f2a69ac265f2
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-12-28 16:08:23 +0530

    Update Message_remover.py

--- root/plugins/Message_remover.py
+++ root/plugins/Message_remover.py
@@ -15,7 +15,7 @@ bad_words = [
 ]
 
 # Build a regex pattern with variations
-pattern = r"\b(?:{})\b".format('|'.join(f'{}(?:{"".join(["{}?".format(c) for c in word])})?'.format(re.escape(word)) for word in bad_words))
+pattern = r"\b(?:{})\b".format('|'.join(f'{}(?:{})?'.format(re.escape(word), '[a-zA-Z]*' * (len(word)-1)) for word in bad_words))
 
 @bot.on_message(filters.text & filters.regex(pattern, re.IGNORECASE))
 async def remove_message(_, message):

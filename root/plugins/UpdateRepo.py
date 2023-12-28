1daf68552e272bb18cf0b955f32d7ed8c96b3576
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-12-28 16:31:49 +0530

    Create UpdateRepo.py

---
+++ root/plugins/UpdateRepo.py
@@ -0,0 +1,19 @@
+from pyrogram import *
+from root.__main__ import *
+from root import *
+import subprocess
+
+@bot.on_message(filters.command("update") & filters.user(OWN))
+async def updaterepo(_, message):
+    await message.reply_text("`Updating Repo..`")
+    try:
+        command = "cd && rm -rf QuantumRoBot && git clone https://GitHub.com/Otazuki004/QuantumRoBot.git && cd QuantumRoBot && ls && python3 -m root"
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        print("Output:", result.stdout)
+        print("Error:", result.stderr)
+        print("Return code:", result.returncode)
+        exit()
+    except Exception as e:
+        await message.reply_text("Update Failed ", e)
+        print("Error", e)
+    

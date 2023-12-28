87870eabe17b8d20525eab53b19830a7af751958
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-12-28 07:42:25 +0530

    Update Webss.py

--- root/plugins/Webss.py
+++ root/plugins/Webss.py
@@ -28,4 +28,4 @@ async def take_ss(_, message):
             else:
                 await message.reply_text("» Usᴀɢᴇ /ᴡᴇʙss ᴡᴡᴡ.(ᴅᴏᴍᴀɪɴɴᴀᴍᴇ).(ᴅᴏᴍᴀɪɴ)")
         except Exception as e:
-            await message.reply_text("Eʀʀᴏʀ: ", e)
+            await message.reply_text(f"Eʀʀᴏʀ: ", e)

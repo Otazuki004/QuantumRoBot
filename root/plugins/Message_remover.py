60d6a9ba4566f7fb14f3c533e65554029b930e49
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-12-28 16:22:50 +0530

    Update Message_remover.py

--- root/plugins/Message_remover.py
+++ root/plugins/Message_remover.py
@@ -1,6 +1,7 @@
 import re
 from pyrogram import filters
 from root.__main__ import bot
+from root import allowbw
 
 # List of words to filter
 bad_words = [
@@ -16,11 +17,11 @@ bad_words = [
 
 # Build a regex pattern with variations
 pattern = r"\b(?:{})\b".format('|'.join(['{}(?:{})?'.format(re.escape(word), '[a-zA-Z]*' * (len(word)-1)) for word in bad_words]))
-
-@bot.on_message(filters.text & filters.regex(pattern, re.IGNORECASE))
-async def remove_message(_, message):
-    try:
-        await message.delete()
-    except Exception as e:
-        print(e)
-        await bot.send_message(message.chat.id, f"Error: {e}")
+if allowbw == True:
+    @bot.on_message(filters.text & filters.regex(pattern, re.IGNORECASE))
+    async def remove_message(_, message):
+        try:
+            await message.delete()
+        except Exception as e:
+            print(e)
+            await bot.send_message(message.chat.id, f"Error: {e}")

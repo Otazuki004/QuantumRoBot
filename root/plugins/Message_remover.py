e485eda5ccebe9add2ce7748f75385dc09b4fd81
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-12-28 14:54:21 +0530

    Update Message_remover.py

--- root/plugins/Message_remover.py
+++ root/plugins/Message_remover.py
@@ -1,11 +1,10 @@
-from pyrogram import *
-from root.__main__ import *
-from root import *
+from pyrogram import filters
+from root.__main__ import bot
 
-@bot.on_message(filters.regex("punda"))
-async def remove_message(client, message):
+@bot.on_message(filters.text & filters.regex(r"\bpunda\b", re.IGNORECASE))
+async def remove_message(_, message):
     try:
         await message.delete()
     except Exception as e:
         print(e)
-        await bot.send_message("Error", e)
+        await bot.send_message(message.chat.id, f"Error: {e}")

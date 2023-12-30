dc11ed7f1cfd1a286c864eedcfc0e914e62d81a1
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-12-30 15:55:31 +0530

    Create main.py

---
+++ root/database/main.py
@@ -0,0 +1,11 @@
+from root import DATABASE
+
+db = DATABASE["MAIN"]
+
+async def add_users_to_db(user_id: int):
+     string = {"user_id": user_id, "coins": 500}
+     db.insert_one(string)
+
+async def get_users_list():
+     list = [x["user_id"] for x in db.find()]
+     return list

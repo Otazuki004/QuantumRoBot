5cbb19da5bfb453a886d57a4b41d4117c690dd64
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-12-30 18:24:37 +0530

    Create Get_logs.py

---
+++ root/plugins/Get_logs.py
@@ -0,0 +1,15 @@
+from pyrogram import *
+from root import bot
+import traceback
+from subprocess import getoutput as run
+
+@bot.on_message(filters.command("logs"))
+async def logs(_, m):
+    run_logs = run("tail log.txt")
+    msg = m.reply_text("Trying to Sending logs...")
+    with io.BytesIO(str.encode(run_logs)) as logs:
+        logs.name = "log.txt"
+        await m.reply_document(
+            document=logs,
+        )
+    await msg.delete()

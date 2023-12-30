5167ce2d5ee64bd6923cf5aad3d93dd5900447af
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-12-30 15:58:29 +0530

    Create profile.py

---
+++ root/database/profile.py
@@ -0,0 +1,23 @@
+from root import DATABASE
+from root.helper.telegraph import telegraph 
+
+db = DATABASE["MAIN"]
+
+async def add_profile_to_users(message, user_id: int, profile):
+     filter = {"user_id": user_id}
+     if await telegraph(message, profile) == False:
+         return           
+     else:
+         link = await telegraph(message, profile)
+         update = {"$set": {"profile": link}}
+         db.update_one(filter, update)
+     
+
+async def get_profile_from_users(user_id: int):
+      string = {"user_id": user_id}
+      mm = db.find_one(string)
+      try:
+         return mm["profile"]
+      except KeyError:
+          return "https://graph.org//file/6b06c18453ebb6e6005da.jpg"
+          

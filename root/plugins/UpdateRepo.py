b58b99bf709d212b8ee62bfa6f22e59ffcc9e7d1
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-12-30 06:33:25 +0530

    Update UpdateRepo.py

--- root/plugins/UpdateRepo.py
+++ root/plugins/UpdateRepo.py
@@ -1,13 +1,13 @@
 from pyrogram import *
 from root.__main__ import *
-from root import OWN
+from root import OWN, ROOTREPO
 import subprocess
 
 @bot.on_message(filters.command("update") & filters.user(OWN))
 async def updaterepo(_, message):
     await message.reply_text("`Updating your bot..`")
     try:
-        command = "cd && rm -rf QuantumRoBot && git clone https://GitHub.com/Otazuki004/QuantumRoBot.git && cd QuantumRoBot && ls && python3 -m root"
+        command = f"cd && rm -rf QuantumRoBot && git clone {ROOTREPO} && cd QuantumRoBot && ls && python3 -m root"
         result = subprocess.run(command, shell=True, capture_output=True, text=True)
         print("Output:", result.stdout)
         print("Error:", result.stderr)

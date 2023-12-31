339b55e5693461a186b5a4066538bcb1ff0098ef
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-12-31 16:10:22 +0530

    Update Restart.py

--- root/plugins/Restart.py
+++ root/plugins/Restart.py
@@ -3,9 +3,13 @@ from root.__main__ import bot
 from pyrogram import *
 from root import OWN
 from root.plugins.code import SRUN
+import subprocess
 
 @bot.on_message(filters.command("restart") & filters.user(OWN))
 def RESTART(_, message):
     bot.send_message(message.chat.id, "Restarting Your Bot..")
-    RUN(f"rm -rf {REPO} && cd {REPO} && python3 -m root")
+    CO = f"rm -rf {REPO} && cd {REPO} && python3 -m root"
+    result = subprocess.run(CO, shell=True, capture_output=True, text=True)
+    print("Output:", result.stdout)
+    print("Error:", result.stderr)
     exit()

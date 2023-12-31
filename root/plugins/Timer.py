49622812699d4a77fecd3d2f33a4738066511ad4
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-12-31 19:10:33 +0530

    Update Timer.py

--- root/plugins/Timer.py
+++ root/plugins/Timer.py
@@ -5,5 +5,5 @@ from pyrogram import *
 @bot.on_message(filters.command("timer15"))
 async def Time15 (_, message):
     await bot.send_message(message.chat.id, "Timer Set ✅")
-    time.sleep(9)
+    await time.sleep(9)
     await message.reply_text("Your Timer Completed ✅")

bd51dbe982555dfec122cedfddf13d7cd885b961
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-12-31 16:12:28 +0530

    Update Restart.py

--- root/plugins/Restart.py
+++ root/plugins/Restart.py
@@ -7,7 +7,7 @@ import subprocess
 @bot.on_message(filters.command("restart") & filters.user(OWN))
 def RESTART(_, message):
     bot.send_message(message.chat.id, "Restarting Your Bot..")
-    CO = f"rm -rf {REPO} && cd {REPO} && python3 -m root"
+    CO = f"cd {REPO} && python3 -m root"
     result = subprocess.run(CO, shell=True, capture_output=True, text=True)
     print("Output:", result.stdout)
     print("Error:", result.stderr)

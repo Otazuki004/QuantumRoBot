aecd4f5549388fee0c5eb423bcba5d3a55bec4e2
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-12-31 20:53:29 +0530

    Update __main__.py

--- root/__main__.py
+++ root/__main__.py
@@ -49,16 +49,8 @@ logging.basicConfig(
 )
 
 RUN(f"git clone {rr}")
-
-
-# Store the start time
-start_time = time.time()
-
-# Calculate uptime in seconds
-uptime_seconds = time.time() - start_time
-
-# Format and print the uptime
-formatted_uptime = "{:.2f}".format(uptime_seconds)
+Ck = f"""await bot.send_message(id, "**I am Started**")"""
+PyRun()
 #Codedown
 
 #NewModuleSongVideo↓

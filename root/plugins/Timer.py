69c3d61734c12ebd15bfdf5c8bda4ef07a5b64a5
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-12-31 19:05:31 +0530

    Update Timer.py

--- root/plugins/Timer.py
+++ root/plugins/Timer.py
@@ -6,5 +6,5 @@ from datetime import *
 @bot.on_message(filters.command("timer15"))
 async def Time15 (_, message):
     await bot.send_message(message.chat.id, "Timer Set ✅")
-    await time.sleep(9)
+    time.sleep(9)
     await message.reply_text("Your Timer Completed ✅")

ef1c12b6efbe958578617f15479bddd67e6d1cb4
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-12-31 17:54:07 +0530

    Create Timer.py

---
+++ root/plugins/Timer.py
@@ -0,0 +1,10 @@
+from root import *
+from root.__main__ import bot
+from pyrogram import *
+from datetime import *
+
+@bot.on_message(filters.command("timer15"))
+async def Time15 (_, messag):
+    await bot.send_message(message.chat.id, "Timer Set ✅")
+    await time.sleep(900)
+    await bot.send_message(message.chat.id, "Your Timer Completed ✅")

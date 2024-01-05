b9094d912691c0a9195ff1b4738c63889a5167ea
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-05 20:30:55 +0530

    Update Get_logs.py

--- root/plugins/Get_logs.py
+++ root/plugins/Get_logs.py
@@ -3,14 +3,16 @@ from pyrogram import *
 from root.__main__ import bot
 import traceback
 from subprocess import getoutput as run
+from pyrogram.enums import ChatAction
 
 @bot.on_message(filters.command("logs"))
-async def logs(_, m):
-    run_logs = run("tail log.txt")
-    msg = m.reply_text("Trying to Sending logs...")
+async def logs(_, message):
+    run_logs = run("cat log.txt")
+    msg = message.reply_text("Trying to Sending logs...")
+    await bot.send_chat_action(message.chat.id, ChatAction.UPLOAD_DOCUMENT)
     with io.BytesIO(str.encode(run_logs)) as logs:
         logs.name = "log.txt"
-        await m.reply_document(
+        await message.reply_document(
             document=logs,
         )
     await msg.delete()

bb7711eb1b344da49d90e9b06f347339bc8d4f31
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-05 11:54:43 +0530

    Update Refrence_code.py

--- root/plugins/Refrence_code.py
+++ root/plugins/Refrence_code.py
@@ -12,29 +12,8 @@ What Happening Now?
 - Repository Created ✅
 - Creating Codes With Gpt-3.5
 - Working Hard
+- I am Suffering With Fever So I can't Make 2 days
 
-Powered by: @Hyper_Speed0**
-"""
-TgBotmk0002Hsv2 = """
-**Valid Code ✅
-
-Bot Status = Created ✅
-
-Information ✅
-- Your Bot Has Been Successfully Created And its Running
-- If Any Problems Ask To @Otazuki Else @FutureCity005
-
-Warranty ⚡
-
-- Your Bot Have 1 Month Warranty
-- Check About Warrenty [Here](https://t.me/Hyper_Speed0/40).
-- Follow That All Rules
-- Your Warranty Time Started On [4 January 2024]
-
-
-Thanks For Using Us
-
-This Code Will Deleted in January 5 12:00PM (IST)
 Powered by: @Hyper_Speed0**
 """
 
@@ -43,11 +22,9 @@ async def refrence(_, message):
     query = " ".join(message.command[1:])
     try:
         if query == "TgBotmk0001Hs":
-            await bot.send_message(message.chat.id, TgBotmk0001Hs)
-        elif query == "TgBotmk0002Hsv2":
-            await bot.send_message(message.chat.id, TgBotmk0002Hsv2)
+            await message.reply_text(f"{TgBotmk0001Hs}")
         else:
-            await bot.send_message(message.chat.id, "Invalid Code ❌")
+            await message.reply_text("Invalid Code ❌")
     except Exception as e:
-        await bot.send_message(message.chat.id, "Went Wrong")
+        await bot.send_message(message.chat.id, f"Somthing Went Wrong: {e}")
         print(e)

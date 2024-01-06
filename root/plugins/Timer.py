0fb2bb4ccdfc1574918462dc3773ec788b3c9db8
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-06 12:16:50 +0530

    Update Timer.py

--- root/plugins/Timer.py
+++ root/plugins/Timer.py
@@ -5,13 +5,15 @@ from pyrogram import *
 @bot.on_message(filters.command("timer"))
 async def timer(_, message):
     try:
+        if len(message.text.split()) <2:
+            return await message.reply_text("Please Enter minutes ⚡")
         mins = int(message.text.split()[1])
     except (IndexError, ValueError):
-        await message.reply("Invalid usage ❌. Use /timer (minutes)")
+        await message.reply("Invalid usage ❌, Use /timer (minutes)")
         return
     if mins <= 0:
         await message.reply("Timer duration should be a positive integer.")
         return
     await message.reply(f"Setting timer for {mins} Minutes ⚡")
     await asyncio.sleep(60 * mins)
-    await message.reply("Your Timer has completed ✅")
+    await message.reply("Your Timer has been completed ✅")

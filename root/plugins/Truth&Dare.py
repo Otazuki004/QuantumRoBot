00d829557f824d6d2351aebcf3bc99b21aa735dd
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-07 14:22:23 +0530

    Update Truth&Dare.py

--- root/plugins/Truth&Dare.py
+++ root/plugins/Truth&Dare.py
@@ -3,4 +3,9 @@ from pyrogram import *
 import random
 
 
-Dare = ['
+Dare = ['Take a screenshot and send it', 'Send Your Browser History Screenshot', 'Tell your Favourite Song Name', 'Send Your Phone Number']
+
+@Client.on_message(filters.command("dare"))
+def dare(_, message):
+    DareR = random.choice(Dare)
+    message.reply_text(DareR)

2a3aba9b34ac67ac55f7bda44691c8a021897d04
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-19 14:52:14 +0530

    Update Get_logs.py

--- root/plugins/Get_logs.py
+++ root/plugins/Get_logs.py
@@ -7,8 +7,19 @@ from pyrogram.enums import ChatAction
 
 @bot.on_message(filters.command("logs"))
 async def logs(_, message):
+    run_logs = run("tail log.txt")
+    text = await message.reply_text("`Getting logs...`")
+    await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+    await message.reply_text(f"```shell\n{run_logs}```")
+    await text.delete()
+
+
+
+
+@bot.on_message(filters.command("flogs"))
+async def logs(_, message):
     run_logs = run("cat log.txt")
-    text = await message.reply_text("Sending logs...")
+    text = await message.reply_text("Sending Full logs...")
     await bot.send_chat_action(message.chat.id, ChatAction.UPLOAD_DOCUMENT)
     with io.BytesIO(str.encode(run_logs)) as logs:
         logs.name = "log.txt"

dd308b53cb70110d0ed65b0fecd80fa783917c0e
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2024-01-22 14:49:18 +0530

    Update __main__.py

--- root/__main__.py
+++ root/__main__.py
@@ -569,8 +569,23 @@ def rename(_, message):
 #NewModuleREPOLINK
 
 @bot.on_message(filters.command("repo"))
-def repo(_, message):
-    bot.send_message(message.chat.id, "GitHub.com/Otazuki004/QuantumRobot")
+async def repo(_, message):
+    await message.reply_text("""**
+Dear Valued User❗,
+
+We extend our heartfelt gratitude for choosing our services. Your support means the world to us. ⚡
+
+Behold, the repository link you've been eagerly waiting for ✅:
+
+Repository Link 🔗 : __https://github.com/Otazuki004/QuantumRoBot.git__
+
+💜 Should you desire further assistance or wish to be part of our vibrant community, don't hesitate to join our esteemed 🗿 [FutureCity Support Group.](https://t.me/FutureCity005)
+
+Embark on a journey of technological marvels at Hyper-Speed ⚡ with us. Explore, engage, and revel in the possibilities that await you.
+
+- Hyper Speed™**
+""", disable_webpage_preview=True)
+
 #RepolinkEND
 
 
